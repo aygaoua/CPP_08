59657200a76da6a4d3c812d1f57c2847ffe86afb
aygaoua <zgaoua72@gmail.com>
2025-03-16 05:42:29 +0000

    make the ex02 clean to push

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
 #include <stack>
 #include <iostream>
@@ -7,22 +8,33 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
 	public:
-	typedef typename std::deque<T>::iterator iterator;
-	typedef typename std::deque<T>::const_iterator const_iterator;
+		MutantStack										(): std::stack<T>() {}
+		MutantStack										(MutantStack const & src): std::stack<T>(src) {}
+		MutantStack&									operator =(const MutantStack& b) {
+			if (this != &b)
+				std::stack<T>::operator=(b);
+			return *this;
+		}
+		~MutantStack									() {}
 
-	iterator		begin() {
-		return this->c.begin();
-	}
+		typedef typename std::deque<T>::iterator		iterator;
+		typedef typename std::deque<T>::const_iterator	const_iterator;
 
-	iterator		end() {
-		return this->c.end();
-	}
+		iterator										begin() {
+			return this->c.begin();
+		}
 
-	const_iterator	begin() const {
-		return this->c.begin();
-	}
+		iterator										end() {
+			return this->c.end();
+		}
 
-	const_iterator	end() const {
-		return this->c.end();
-	}
+		const_iterator									begin() const {
+			return this->c.begin();
+		}
+
+		const_iterator									end() const {
+			return this->c.end();
+		}
 };
+
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,55 +3,54 @@
 
 int main()
 {
-    std::cout << "_________MutantStack__________\n";
-    {
-        MutantStack<int> mstack;
-        mstack.push(5);
-        mstack.push(17);
-        std::cout <<  "top:          " << mstack.top() << std::endl;
-        mstack.pop();
-        std::cout <<  "size:         " << mstack.size() << std::endl;
-        mstack.push(3);
-        mstack.push(5);
-        mstack.push(737);
-        mstack.push(1337);
-        mstack.push(0);
-        MutantStack<int>::iterator it = mstack.begin();
-        MutantStack<int>::iterator ite = mstack.end();
-        ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout <<  "              " << *it << std::endl;
-            ++it;
-        }
-        std::stack<int> s(mstack);
-    }
-    std::cout << std::endl;
-    std::cout << "_____________list_____________\n" ;
-    {
-        std::list<int> myList;
-        myList.push_back(5);
-        myList.push_back(17);
-        std::cout <<  "top:          " << myList.back() << std::endl;
-        myList.pop_back();
-        std::cout <<  "size:         " << myList.size() << std::endl;
-        myList.push_back(3);
-        myList.push_back(5);
-        myList.push_back(737);
-        myList.push_back(1337);
-        myList.push_back(0);
-        std::list<int>::iterator it = myList.begin();
-        std::list<int>::iterator ite = myList.end();
-        ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout << "              " << *it << std::endl;
-            ++it;
-        }
-        std::stack<int, std::list<int> > ss(myList);
-    }
-    return 0;
-
+	std::cout << "_________MutantStack__________\n";
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout <<  "top:          " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout <<  "size:         " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(1337);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout <<  "              " << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::endl;
+	std::cout << "_____________list_____________\n" ;
+	{
+		std::list<int> myList;
+		myList.push_back(5);
+		myList.push_back(17);
+		std::cout <<  "top:          " << myList.back() << std::endl;
+		myList.pop_back();
+		std::cout <<  "size:         " << myList.size() << std::endl;
+		myList.push_back(3);
+		myList.push_back(5);
+		myList.push_back(737);
+		myList.push_back(1337);
+		myList.push_back(0);
+		std::list<int>::iterator it = myList.begin();
+		std::list<int>::iterator ite = myList.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << "              " << *it << std::endl;
+			++it;
+		}
+		std::stack<int, std::list<int> > ss(myList);
+	}
+	return 0;
 }
