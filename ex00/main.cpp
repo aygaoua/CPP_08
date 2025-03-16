563c32d6bdc4cdf9100f5a316542a140a4ef2c27
aygaoua <zgaoua72@gmail.com>
2025-03-16 05:04:42 +0000

    make the main good to push

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -12,18 +12,26 @@ int main() {
     vec.push_back(3);
     vec.push_back(4);
     vec.push_back(5);
+    std::cout << "_____________vector_____________\n";
+    std::cout << "           ";
     std::for_each(vec.begin(), vec.end(), printo);
     std::cout << std::endl;
+
+    std::cout << std::endl;
     try {
-        std::vector<int>::iterator it = easyfind(vec, 5);
-        std::cout << "Element found in the index: " << std::distance(vec.begin(), it) << std::endl;
+        std::cout << "_____________test1_____________\n";
+        std::vector<int>::iterator it1 = easyfind(vec, 5);
+        std::cout << "Element found in the index: " << std::distance(vec.begin(), it1) << std::endl;
+        std::cout << std::endl;
+
     } catch (const std::runtime_error& e) {
         std::cout << e.what() << std::endl;
     }
 
     try {
-        std::vector<int>::iterator it = easyfind(vec, 1);
-        std::cout << "Element found in the index: " << std::distance(vec.begin(), it) << std::endl;
+        std::cout << "_____________test2_____________\n";
+        std::vector<int>::iterator it2 = easyfind(vec, 1);
+        std::cout << "Element found in the index: " << std::distance(vec.begin(), it2) << std::endl;
     } catch (const std::runtime_error& e) {
         std::cout << e.what() << std::endl;
     }
